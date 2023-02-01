16db36b2c1d4869ea9dbb481ebe85bd92f465ba8
faaizhr <faizhafizh28@gmail.com>
2023-02-01 13:52:45 +0700

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="cotainer mx-auto lg:px-24 sm:px-12">
+        <div class="flex justify-between">
+            <div>
+                <h1>Dentsu</h1>
+            </div>
+            <div>
+                <h1>Indonesia</h1>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  
+export default {
+  name: "Navbar"
+}
+  </script>
+  
+  <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <Navbar/>
+  </div>
 </template>
